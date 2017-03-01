76730eb456aab4efb552d422bed3490c47a578b8
infinite-yy <infiniteyy1104@gmail.com>
2017-03-01 15:40:49 +0800

    update

--- js/typing.js
+++ js/typing.js
@@ -1,26 +1,68 @@
 (function ($) {
-  // Caption
-  $('.article-entry').each(function (i) {
-    $(this).find('img').each(function () {
-      if ($(this).parent().hasClass('fancybox')) return
+  // set fancy box for images in post
+  // --------------------------------------------------------
+    $('.article-entry').each(function (i) {
+      $(this).find('img').each(function () {
+        if ($(this).parent().hasClass('fancybox')) return
 
-      var alt = this.alt
+        var alt = this.alt
 
-      if (alt) {
-        $(this).after('<span class="caption">' + alt +
-                      '</span>')
-      }
+        if (alt) {
+          $(this).after('<span class="caption">' + alt +
+                        '</span>')
+        }
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt +
-                   '" class="fancybox"></a>')
+        $(this).wrap('<a href="' + this.src + '" title="' + alt +
+                    '" class="fancybox"></a>')
+      })
+
+      $(this).find('.fancybox').each(function () {
+        $(this).attr('rel', 'article' + i)
+      })
     })
+    if ($.fancybox) {
+      $('.fancybox').fancybox()
+    }
+  // --------------------------------------------------------
 
-    $(this).find('.fancybox').each(function () {
-      $(this).attr('rel', 'article' + i)
-    })
-  })
+  // initiation all page
+  init();
+  var nowUrl = '';
+  var menuItem;
+  function init(){
+    nowUrl = window.location.pathname.replace(/\//g,'');
 
-  if ($.fancybox) {
-    $('.fancybox').fancybox()
+    initMenu();
   }
+
+  function initMenu(){
+    menuItem = $('.main-nav-link');
+    if(menuItem.length > 0){
+      for(var i = 0,l = menuItem.length; i<l; i++){
+        var tmpMenuItem =  $(menuItem[i]);
+        var tmpModuleName = tmpMenuItem.attr('href').replace(/\//g,'');
+
+        if(tmpModuleName == nowUrl){
+          tmpMenuItem.addClass('disable');
+        }
+
+        tmpMenuItem.on('click touchstart', function(e){
+          if($(this).hasClass('disable')){
+            e.stopPropagation();
+            return false;
+          }
+        });
+
+        /*
+        * hide about page temporarily cos i'am not prepared
+        */
+        if(tmpModuleName == 'about'){
+          tmpMenuItem.addClass('disable');
+        }
+      }
+    }
+  }
+
+
+
 })(jQuery)
